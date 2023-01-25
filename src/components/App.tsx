4ba3314aef7cb70b419e2a328482a5aa1ae3e53f
neaguio <mihaicosminneagu@gmail.com>
2023-01-26 00:00:57 +0200

    fix: useState => React.useState

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,10 +1,10 @@
-import React, { useState } from 'react'
+import React from 'react'
 
 type Props = {
   value?: number
 }
 const MyCounter = ({ value = 0 }: Props) => {
-  const [counter, setCounter] = useState(value)
+  const [counter, setCounter] = React.useState(value)
 
   const onMinus = () => {
     setCounter((prev) => prev - 1)
