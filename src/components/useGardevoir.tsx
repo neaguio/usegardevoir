f86773aa62359c11b1a85f1dcd476da0c03481cc
neaguio <mihaicosminneagu@gmail.com>
2023-01-28 21:00:39 +0200

    chore : remove commented code

--- src/components/useGardevoir.tsx
+++ src/components/useGardevoir.tsx
@@ -4,54 +4,16 @@ import { SWRConfiguration } from 'swr'
 
 const fetcher = (url: string) => fetch(url).then((res) => res.json())
 
-// export interface ConfigType {
-//   property1: string;
-//   property2: string;
-// }
-
-// export default function GardevoirInitialize(config: { [key: string]: any }) {
-//   const findApi = React.useCallback(
-//     (
-//       api: keyof typeof config,
-//       {
-//         query = {},
-//         customConfig = {},
-//       }: {
-//         query?: { [key: string]: any }
-//         customConfig?: SWRConfiguration
-//       } = {},
-//     ) => {
-//       const swrConfigFn = config?.[api]
-
-//       if (typeof swrConfigFn === 'function') {
-//         const { url, ...rest } = swrConfigFn(query, customConfig)
-//         // eslint-disable-next-line react-hooks/rules-of-hooks
-//         return useSWR(url, fetcher, {
-//           ...rest,
-//         })
-//       }
-//       // eslint-disable-next-line react-hooks/rules-of-hooks
-//       if (typeof api === 'string') return useSWR(api, fetcher, {})
-
-//       return api;
-//     },
-//     [config],
-//   )
-//   return findApi
-// }
-
 interface SWRConfig {
   url: string
   [key: string]: any
 }
 
-
-export default function GardevoirInitialize<T extends { [key: string]: (query?: { [key: string]: any }, customConfig?: SWRConfiguration) => SWRConfig }>(config: T) {
+export default function GardevoirInitialize<
+  T extends { [key: string]: (query?: { [key: string]: any }, customConfig?: SWRConfiguration) => SWRConfig },
+>(config: T) {
   const findApi = React.useCallback(
-    (
-      api: keyof T,
-      options: { query?: { [key: string]: any }; customConfig?: SWRConfiguration } = {},
-    ) => {
+    (api: keyof T, options: { query?: { [key: string]: any }; customConfig?: SWRConfiguration } = {}) => {
       const swrConfigFn = config?.[api]
 
       if (typeof swrConfigFn === 'function') {
