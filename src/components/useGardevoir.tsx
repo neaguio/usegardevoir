7484e11668a3863e329f59ce2a0426c8d0e7cf60
neaguio <mihaicosminneagu@gmail.com>
2023-01-28 20:52:24 +0200

    fix: ConfigType

--- src/components/useGardevoir.tsx
+++ src/components/useGardevoir.tsx
@@ -4,26 +4,63 @@ import { SWRConfiguration } from 'swr'
 
 const fetcher = (url: string) => fetch(url).then((res) => res.json())
 
-export default function GardevoirInitialize(config: { [key: string]: any }) {
+// export interface ConfigType {
+//   property1: string;
+//   property2: string;
+// }
+
+// export default function GardevoirInitialize(config: { [key: string]: any }) {
+//   const findApi = React.useCallback(
+//     (
+//       api: keyof typeof config,
+//       {
+//         query = {},
+//         customConfig = {},
+//       }: {
+//         query?: { [key: string]: any }
+//         customConfig?: SWRConfiguration
+//       } = {},
+//     ) => {
+//       const swrConfigFn = config?.[api]
+
+//       if (typeof swrConfigFn === 'function') {
+//         const { url, ...rest } = swrConfigFn(query, customConfig)
+//         // eslint-disable-next-line react-hooks/rules-of-hooks
+//         return useSWR(url, fetcher, {
+//           ...rest,
+//         })
+//       }
+//       // eslint-disable-next-line react-hooks/rules-of-hooks
+//       if (typeof api === 'string') return useSWR(api, fetcher, {})
+
+//       return api;
+//     },
+//     [config],
+//   )
+//   return findApi
+// }
+
+interface SWRConfig {
+  url: string
+  [key: string]: any
+}
+
+interface GardevoirInitializeConfig {
+  [key: string]: (query?: { [key: string]: any }, customConfig?: SWRConfiguration) => SWRConfig
+}
+
+export default function GardevoirInitialize(config: GardevoirInitializeConfig) {
   const findApi = React.useCallback(
     (
-      api: keyof typeof config,
-      {
-        query = {},
-        customConfig = {},
-      }: {
-        query?: { [key: string]: any }
-        customConfig?: SWRConfiguration
-      } = {},
+      api: keyof GardevoirInitializeConfig,
+      options: { query?: { [key: string]: any }; customConfig?: SWRConfiguration } = {},
     ) => {
       const swrConfigFn = config?.[api]
 
       if (typeof swrConfigFn === 'function') {
-        const { url, ...rest } = swrConfigFn(query, customConfig)
+        const { url, ...rest } = swrConfigFn(options.query, options.customConfig)
         // eslint-disable-next-line react-hooks/rules-of-hooks
-        return useSWR(url, fetcher, {
-          ...rest,
-        })
+        return useSWR(url, fetcher, rest)
       }
       // eslint-disable-next-line react-hooks/rules-of-hooks
       if (typeof api === 'string') return useSWR(api, fetcher, {})
@@ -32,5 +69,6 @@ export default function GardevoirInitialize(config: { [key: string]: any }) {
     },
     [config],
   )
+
   return findApi
 }
